ae65734ee5888d00d9be68a7f1806140e900a190
MeCookedInMath <clavallee066@gmail.com>
2024-11-21 16:50:21 -0500

    update projetBDD.sql

--- Projet_BDD/BDD_projet.sql
+++ Projet_BDD/BDD_projet.sql
@@ -87,10 +87,26 @@ CREATE TABLE Seances (
     date DATE ,
     heure TIME,
     nbr_places INT,
-    note INT,
+    note INT null,
     primary key (id),
     foreign key (nom_activite) references Activites(nom)
 );
+
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Canot-Camping', '2024-11-21', '14:00:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Escalade', '2024-12-04', '13:20:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Canot-Camping', '2024-08-21', '13:30:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Peintures', '2024-10-15', '12:20:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Volleyball', '2024-01-17', '12:10:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Tyrolienne', '2024-01-20', '11:20:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Via ferrata', '2024-02-02', '12:20:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Canot-Camping', '2024-11-22', '13:15:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Dentifrice d`éléphants', '2024-11-24', '12:25:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Canot-Camping', '2024-04-30', '10:40:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Équitation', '2024-11-30', '11:45:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Équitation', '2024-07-23', '13:10:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Cirque', '2024-11-21', '10:20:00' , 20, null);
+INSERT INTO Seances (nom_activite, date, heure, nbr_places, note) values ('Club de lecture', '2024-11-21', '09:20:00' , 20, null);
+
 
 
 
@@ -102,6 +118,10 @@ CREATE TABLE Inscriptions (
     foreign key (id_adherent) references Adherents(no_identification),
     FOREIGN KEY (id_seance) references Seances(id)
 );
+
+INSERT INTO Inscriptions (id_adherent, id_seance) values ('AA-2005-623', 1);
+
+
 
 
 
@@ -131,5 +151,60 @@ CREATE TRIGGER creer_numeroIdentification
         SET NEW.no_identification = CONCAT(SUBSTRING(NEW.prenom, 1, 1), SUBSTRING(NEW.nom, 1, 1), '-', YEAR(NEW.date_naissance), '-', FLOOR(1+(RAND()*(9 - 1 + 1))),  FLOOR(1+(RAND()*(9 - 1 + 1))),  FLOOR(1+(RAND()*(9 - 1 + 1)))   );
     end //
 DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER gerer_nbrPlaces_seances
+    AFTER INSERT ON Inscriptions
+    FOR EACH ROW
+    BEGIN
+        DECLARE nbrPlaces INT ;
+        SET nbrPlaces = (SELECT nbr_places FROM Seances where id = NEW.id_seance) - 1;
+
+        UPDATE Seances SET nbr_places = nbrPlaces WHERE id = NEW.id_seance;
+
+
+    end //
+DELIMITER ;
+
+
+DELIMITER //
+CREATE TRIGGER verifier_nombrePlaces_seances
+    BEFORE INSERT ON Inscriptions
+    FOR EACH ROW
+    BEGIN
+        IF (SELECT nbr_places FROM Seances WHERE id = NEW.id_seance) = 0 THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'Un Participant ne peut pas être ajouté. Le nombre de places restantes est de 0.';
+
+
+        end if ;
+    end //
+DELIMITER ;
+
+
+/* Création des procédures */
+DELIMITER //
+CREATE PROCEDURE insertion_inscriptions (IN idDeAdherent VARCHAR(11), IN idDeSeances INT)
+BEGIN
+    INSERT INTO Inscriptions (id_adherent, id_seance) VALUES (idDeAdherent, idDeSeances);
+end //
+DELIMITER ;
+
+
+
+/* Création des fonctions */
+
+/* !!!!! pas fini !!!!!*/
+DELIMITER //
+CREATE FUNCTION NbrTotal_Adherents (nomActivite VARCHAR(11)) RETURNS INT
+BEGIN
+    SELECT COUNT(I.id_adherent)
+    FROM Activites
+    INNER JOIN Seances S on Activites.nom = S.nom_activite
+    INNER JOIN Inscriptions I on S.id = I.id_seance
+    GROUP BY S.id;
+end //
+DELIMITER ;
+
 
 
